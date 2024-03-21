ac36f8433145c29c6e9ff506df49cd4b1f79cbc9
khalil <khalil.farashiani@zoodfood.com>
2024-03-21 14:39:28 +0330

    :zap: add prettier to make a pretty response  table

--- go.mod
+++ go.mod
@@ -7,4 +7,10 @@ require (
 	github.com/peterbourgon/ff/v4 v4.0.0-alpha.4
 )
 
-require github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+require (
+	github.com/jedib0t/go-pretty/v6 v6.5.5 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
---
+++ prettier.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/jedib0t/go-pretty/v6/table"
+	"os"
+	"reflect"
+)
+
+func makeTable(data []interface{}) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	if len(data) > 0 {
+		var tr = table.Row{}
+		switch reflect.TypeOf(data[0]).Kind() {
+		case reflect.Struct:
+			val := reflect.ValueOf(data[0])
+			for i := 0; i < val.NumField(); i++ {
+				tr = append(tr, val.Type().Field(i).Name)
+			}
+			t.AppendHeader(tr)
+			t.AppendSeparator()
+		}
+	}
+	for _, d := range data {
+		var tr = table.Row{}
+		val := reflect.ValueOf(d)
+		for i := 0; i < val.NumField(); i++ {
+			tr = append(tr, val.Field(i).Interface())
+		}
+		t.AppendHeader(tr)
+		t.AppendSeparator()
+	}
+	t.SetStyle(table.StyleColoredBright)
+	t.Render()
+}
+
+func toSlice(i interface{}) []interface{} {
+	var out []interface{}
+
+	rv := reflect.ValueOf(i)
+	if rv.Kind() == reflect.Slice {
+		for i := 0; i < rv.Len(); i++ {
+			out = append(out, rv.Index(i).Interface())
+		}
+	}
+	return out
+}
